bf471389de7f4c6e8c121b75eba98ca2872bda06
Josh Wells <wellsy9722@gmail.com>
2023-10-06 19:00:56 +1100

    Implemented movement for pacstudent

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -6,6 +6,14 @@ public class PacStudentController : MonoBehaviour
 {
     private Animator animator;
 
+    private const int UP = 2;
+    private const int DOWN = 1;
+    private const int LEFT = 3;
+    private const int RIGHT = 4;
+
+    private int currentDirection = UP;  // Starting direction
+    private float speed = 5.0f;  // Movement speed,
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -18,28 +26,88 @@ public class PacStudentController : MonoBehaviour
 
     void HandleMovement()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+
+        switch (currentDirection)
         {
-            animator.SetInteger("Direction", 2);
-            // TODO
+            case UP:
+                MoveUp();
+                // TODO: Add condition to change direction to RIGHT
+                break;
+            case DOWN:
+                MoveDown();
+                // TODO: Add condition to change direction to LEFT
+                break;
+            case LEFT:
+                MoveLeft();
+                // TODO: Add condition to change direction to UP
+                break;
+            case RIGHT:
+                MoveRight();
+                // TODO: Add condition to change direction to DOWN
+                break;
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            animator.SetInteger("Direction", 1);
-            // TODO
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            animator.SetInteger("Direction", 3);
-            // TODO
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            animator.SetInteger("Direction", 4);
-            // TODO
-        }
-        
+
+        senseDirectionChange(); // Check for direction change
     }
+
+    private void senseDirectionChange()
+    {
+        // Fetch the current position of PacStudent
+        Vector3 currentPosition = transform.position;
+
+        // Check conditions to change direction based on PacStudent's position
+        if (currentDirection == RIGHT && IsCloseTo(currentPosition, -53.3f, 84f))
+        {
+            currentDirection = DOWN;
+        }
+        else if (currentDirection == DOWN && IsCloseTo(currentPosition, -53.3f, 60f))
+        {
+            currentDirection = LEFT;
+        }
+        else if (currentDirection == LEFT && IsCloseTo(currentPosition, -84f, 60f))
+        {
+            currentDirection = UP;
+        }
+        else if (currentDirection == UP && IsCloseTo(currentPosition, -84f, 84f))
+        {
+            Debug.Log("TURN RIGHT");
+            currentDirection = RIGHT;
+        }
+    }
+
+
+    private bool IsCloseTo(Vector3 currentPosition, float targetX, float targetY, float range = 1f)
+    {
+        return Mathf.Abs(currentPosition.x - targetX) <= range && Mathf.Abs(currentPosition.y - targetY) <= range;
+    }
+
+
+
+    private void MoveUp()
+    {
+        animator.SetInteger("Direction", UP);
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+
+    private void MoveDown()
+    {
+        animator.SetInteger("Direction", DOWN);
+        transform.Translate(transform.up * -speed * Time.deltaTime);
+    }
+
+    private void MoveLeft()
+    {
+        animator.SetInteger("Direction", LEFT);
+        transform.Translate(transform.right * speed * Time.deltaTime);
+    }
+
+    private void MoveRight()
+    {
+        animator.SetInteger("Direction", RIGHT);
+        transform.Translate(transform.right * -speed * Time.deltaTime);
+    }
+
+
 
     // Example of setting the IsDead parameter:
     // This can be triggered by other events, such as collision with ghosts.
