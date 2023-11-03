31f9622e41461370e236a1597a0b2c57402a5868
Josh Wells <wellsy9722@gmail.com>
2023-11-03 16:14:37 +1100

    Assigned teleporter objects

---
+++ Assets/MapManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    // Array of size 2 of gameobjects 
+    public GameObject[] leftTeleporters = new GameObject[2];
+    public GameObject[] rightTeleporters = new GameObject[2];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
